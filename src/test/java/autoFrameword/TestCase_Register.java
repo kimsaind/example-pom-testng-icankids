b94424abb9a2a62c096e6107a9544e86f2efad8e
kimsaind <kimphung851997@gmail.com>
2021-09-24 17:58:42 +0700

    Update full test case Register

--- src/test/java/autoFrameword/TestCase_Register.java
+++ src/test/java/autoFrameword/TestCase_Register.java
@@ -6,10 +6,13 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import POM.Login_Page;
 import appModule.Login_Action;
 
 // Chứa testcase
@@ -20,6 +23,8 @@ public class TestCase_Register {
 
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
+	
+	Actions action;
 
 	@BeforeMethod
 	public void setUp() {
@@ -35,63 +40,125 @@ public class TestCase_Register {
 
 		driver.manage().timeouts().implicitlyWait(22, TimeUnit.SECONDS);
 		driver.get("https://id.dev.icankids.com.vn/auth");
+		
+		action = new Actions(driver);
 
 	}
 
-	@Test
-	public void TC_01_Null_Phone_Number_Invalid() {
+	//@Test (description = "Không nhập số điện thoại")
+	public void TC_01_Do_Not_Phone_Number() {
+		
+		// Click button Tiếp tục
+	
+		action.click(Login_Page.btn_Submit(driver)).perform();
+		
+		//Assert.assertEquals(driver.getCurrentUrl(), "https://id.dev.icankids.com.vn/auth");
+	}
+	//@Test (description = "Đúng định dạng số điện thoại")
+	public void TC_02_Phone_Number_Is_Correct() {
+		
+		// Click button Tiếp tục
+		Login_Action.inputPhone(driver, "0987123499");
+		Login_Action.btnSubmit(driver);
+		sleepInSecond(1);
+		
+		Assert.assertTrue(driver.findElement(By.cssSelector("input#phone-otp-2")).isDisplayed());
+	}
+	//@Test (description = "Sai định dạng số điện thoại")
+	public void TC_03_Phone_Number_InCorrect() {
+		
+		// Click button Tiếp tục
+		Login_Action.inputPhone(driver, "1234567890");
+		Login_Action.btnSubmit(driver);
+		sleepInSecond(1);
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//small[@id='textErrorPhone']")).getText(),
+				"Số điện thoại chưa hợp lệ. Vui lòng thử lại");
+	}
+	//@Test (description = "Nhập đầu số khác đầu số VN")
+	public void TC_04_Enter_International_Phone_Number() {
 
 		// Click button Tiếp tục
+		Login_Action.inputPhone(driver, "25212334512");
 		Login_Action.btnSubmit(driver);
-		Assert.assertEquals(driver.getCurrentUrl(), "https://id.dev.icankids.com.vn/auth");
+		sleepInSecond(1);
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//small[@id='textErrorPhone']")).getText(),
+				"Số điện thoại chưa hợp lệ. Vui lòng thử lại");
 	}
 
-	@Test
-	public void TC_02_Check_Phone_Number_Front_Desk() {
+	//@Test (description = "Nhập số điện thoại bàn")
+	public void TC_05_Check_Phone_Number_Front_Desk() {
 
 		// Input phone
 		Login_Action.inputPhone(driver, "0281234567");
 		Login_Action.btnSubmit(driver);
+		sleepInSecond(1);
+		
 		Assert.assertEquals(driver.findElement(By.xpath("//small[@id='textErrorPhone']")).getText(),
 				"Số điện thoại chưa hợp lệ. Vui lòng thử lại");
 
 	}
-
-	@Test
-	public void TC_03_Check_Large_phone_number_10() {
+	//@Test (description = "Nhập số điện thoại > 10 ")
+	public void TC_06_Check_Large_phone_number_10() {
 
 		// Input phone
 		Login_Action.inputPhone(driver, "09871234567");
 		Login_Action.btnSubmit(driver);
+		sleepInSecond(1);
+		
 		Assert.assertEquals(driver.findElement(By.xpath("//small[@id='textErrorPhone']")).getText(),
 				"Số điện thoại chưa hợp lệ. Vui lòng thử lại");
 	}
 
-	@Test
-	public void TC_04_Check_Small_phone_number_9() {
+	//@Test (description = "Nhập số điện thoại < 10 ")
+	public void TC_07_Check_Small_phone_number_9() {
 		// Input phone
 		Login_Action.inputPhone(driver, "09871234");
 		Login_Action.btnSubmit(driver);
+		sleepInSecond(1);
+		
 		Assert.assertEquals(driver.findElement(By.xpath("//small[@id='textErrorPhone']")).getText(),
 				"Số điện thoại chưa hợp lệ. Vui lòng thử lại");
 	}
 
-	@Test
-	public void TC_05_Valid_Telephone_Number() {
+	//@Test (description = "Không nhập otp")
+	public void TC_08_Otp_Should_Be_Avoided() {
 		// Input phone
 		Login_Action.inputPhone(driver, "0987123499");
+		Login_Action.btnSubmit(driver);
+		
+		sleepInSecond(1);
+		
+		// Input otp
+		Login_Action.inputOtp(driver, "");
+		Login_Action.btnSubmit(driver);
+		Assert.assertEquals(driver
+				.findElement(By.xpath("//small[contains(text(),'Vui lòng nhập đúng mã OTP bạn nhận được qua SMS!')]"))
+				.getText(), "Vui lòng nhập đúng mã OTP bạn nhận được qua SMS!");
+	}
+	//@Test (description = "Nhập sai otp")
+	public void TC_08_Otp_Incorrect() {
+		// Input phone
+		Login_Action.inputPhone(driver, "0987123499");
+		Login_Action.btnSubmit(driver);
+		sleepInSecond(1);
+		
+		// Input otp
+		Login_Action.inputOtp(driver, "1234");
 		Login_Action.btnSubmit(driver);
 		Assert.assertEquals(driver
 				.findElement(By.xpath("//small[contains(text(),'Vui lòng nhập đúng mã OTP bạn nhận được qua SMS!')]"))
 				.getText(), "Vui lòng nhập đúng mã OTP bạn nhận được qua SMS!");
 	}
 
-	@Test
-	public void TC_06_Check_Otp_3_Invalid() {
+	//@Test (description = "Nhập otp < 4")
+	public void TC_09_Check_Small_Otp_4() {
 		// Input phone
 		Login_Action.inputPhone(driver, "0987123499");
 		Login_Action.btnSubmit(driver);
-
+		sleepInSecond(1);
+		
 		// Input otp
 		Login_Action.inputOtp(driver, "123");
 		Assert.assertEquals(driver
@@ -99,25 +166,29 @@ public class TestCase_Register {
 				.getText(), "Vui lòng nhập đúng mã OTP bạn nhận được qua SMS!");
 	}
 
-	@Test
-	public void TC_07_Check_Otp_4_Invalid() {
+	//@Test (description = "Nhập otp > 4")
+	public void TC_10_Check_Large_Otp_4() {
 		// Input phone
 		Login_Action.inputPhone(driver, "0987123499");
 		Login_Action.btnSubmit(driver);
+		sleepInSecond(1);
 
 		// Input otp
-		Login_Action.inputOtp(driver, "1234");
+		Login_Action.inputOtp(driver, "12345");
 		Login_Action.btnSubmit(driver);
 		Assert.assertEquals(
 				driver.findElement(By.xpath("//small[contains(text(),'Thông tin chứng thực không hợp lệ')]")).getText(),
 				"Thông tin chứng thực không hợp lệ");
 	}
 
-	@Test
-	public void TC_00_Register_Success() {
-
+	@Test (description = "Nhập otp đúng")
+	public void TC_11_Otp_Is_Correct() {
+		// Input phone
 		Login_Action.inputPhone(driver, phone);
 		Login_Action.btnSubmit(driver);
+		sleepInSecond(1);
+		
+		// Input otp
 		Login_Action.inputOtp(driver, otp);
 		Login_Action.btnSubmit(driver);
 		Assert.assertEquals(driver.getTitle(), "https://id.dev.icankids.com.vn/ho-so/tai-khoan/");
